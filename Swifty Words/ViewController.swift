7ec3033c330ee5c56693d7d1c367cd84923f20ec
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-06-18 12:15:16 +0300

    Added tiny borders to letter buttons

--- Swifty Words/ViewController.swift
+++ Swifty Words/ViewController.swift
@@ -135,6 +135,9 @@ class ViewController: UIViewController {
 
                 // give the button some temporary text so we can see it on-screen
                 letterButton.setTitle("WWW", for: .normal)
+                letterButton.layer.borderWidth = 1
+                letterButton.layer.borderColor = UIColor.lightGray.cgColor
+                letterButton.layer.cornerRadius = 8
 
                 // calculate the frame of this button using its column and row
                 let frame = CGRect(x: col * width, y: row * height, width: width, height: height)
